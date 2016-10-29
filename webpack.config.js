b79ce623d747bf5fbc6bd1ef831d6838adb65868
Michael Pontus <m.pontus@gmail.com>
2016-10-29 19:40:21 +0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./src/index.jsx",
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    }],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
