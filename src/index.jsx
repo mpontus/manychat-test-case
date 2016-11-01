1ad23232fc844086258ac5674789277c1fd87a1c
Michael Pontus <m.pontus@gmail.com>
2016-11-01 07:34:35 +0300

    Extract fakeComment loop to utils library

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import Chance from 'chance';
 import reducer from './reducers';
-import {setComments, addComment} from './actions';
+import { setComments, addComment } from './actions';
 import * as api from './api';
 import App from './components/App';
-import {generateAvatarUrl} from './utils/avatar';
+import { generateAvatarUrl } from './utils/avatar';
+import { createFakeComment, fakeCommentLoop } from './utils/fake';
 import './stylesheets/main.scss';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -24,7 +25,7 @@ const store = createStore(reducer, {
 
 const pollComments = (since) => {
   setTimeout(() => api.pollComments(since).then(comments => {
-    comments.forEach(comment => store.dispatch(addComment(comment)));
+    comments.forEach(comment => store.dispatch(addComment(comment, comment.parentId)));
     pollComments(Date.now());
   }), 1000);
 }
@@ -32,34 +33,11 @@ const pollComments = (since) => {
 api.fetchComments()
   .then(comments => {
     store.dispatch(setComments(comments))
-//    pollComments(Date.now());
+    pollComments(Date.now());
   });
 
-const fakeCommentLoop = (() => {
-  const flatten = (tree, fn) =>
-    tree.reduce((acc, cur) => [...acc, cur, ...flatten(fn(cur), fn)], []);
-
-  const randomInt = (max) => Math.floor(Math.random() * max);
-
-  const chance = new Chance();
-
-  return () => {
-    setTimeout(() => {
-      api.fetchComments().then(comments => {
-        const flatComments = flatten(comments, c => c.replies || []);
-        const parentIndex = randomInt(flatComments.length - 1) + 1;
-        const parentId = parentIndex ? flatComments[parentIndex - 1].id : null;
-        const username = chance.name();
-        api.createComment({
-          username: username,
-          avatarUrl: generateAvatarUrl(username),
-        }, chance.sentence(), parentId).then(() => fakeCommentLoop());
-      });
-    }, 1000);
-  }
-})();
-
-fakeCommentLoop();
+window.createFakeComment = createFakeComment;
+// fakeCommentLoop();
 
 ReactDOM.render(
   <Provider store={store}>
