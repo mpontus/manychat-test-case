58dbd35181a1d7c5de5317b8364e9d60dc4f90ba
Michael Pontus <m.pontus@gmail.com>
2016-11-05 17:04:58 +0300

    Add comment animations

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import CommentItem from './CommentItem';
 
 const CommentList = ({ comments }) => (
   <ul className="comment-list">
-    {comments.map(comment => (
-       <CommentItem key={comment.id} comment={comment} />
-     ))}
+    <ReactCSSTransitionGroup
+      transitionName="comment-animation"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={500}>
+      {comments.map(comment => (
+         <CommentItem key={comment.id} comment={comment} />
+       ))}
+    </ReactCSSTransitionGroup>
   </ul>
 );
 
