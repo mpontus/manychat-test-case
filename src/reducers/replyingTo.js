e054a8aeb44a16d5848f0483c3e103cea2d93733
Michael Pontus <m.pontus@gmail.com>
2016-11-06 18:59:18 +0300

    Reset replyingTo when removing original comment

--- src/reducers/replyingTo.js
+++ src/reducers/replyingTo.js
@@ -1,9 +1,12 @@
 import {
   SET_REPLYING_TO,
+  REMOVE_COMMENT,
 } from '../constants';
 
 const replyingTo = (state = null, action) => {
   switch (action.type) {
+    case REMOVE_COMMENT:
+      return action.comment.id === state ? null : state;
     case SET_REPLYING_TO:
       return action.commentId;
     default:
