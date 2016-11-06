aaaa11dec68669d0852aac212999fd107ec47888
Michael Pontus <m.pontus@gmail.com>
2016-11-06 18:14:12 +0300

    Enable collapsing of comment text

--- src/components/CommentText.jsx
+++ src/components/CommentText.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FormattedText from './FormattedText';
+import { connectToConfiguration } from '../utils/configuration';
 
-const CommentText = ({text}) => (
-  <div className="comment-text">
-    <FormattedText text={text} />
-  </div>
-);
+class CommentText extends Component {
+  static propTypes = {
+    text: React.PropTypes.string.isRequired,
+    collapsedLength: React.PropTypes.number,
+  };
 
-export default CommentText;
+  static defaultProps = {
+    collapsedLength: null,
+  }
+
+  constructor(props) {
+    super();
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  isExpanded() {
+    return this.state.expanded;
+  }
+
+  canBeExpanded() {
+    const { collapsedLength, text } = this.props;
+    return collapsedLength && text.length > collapsedLength;
+  }
+
+  toggleExpanded() {
+    this.setState({
+      expanded: !this.isExpanded(),
+    });
+  }
+
+  getVisibleText() {
+    if (!this.canBeExpanded() || this.state.expanded) {
+      return this.props.text;
+    }
+    const { collapsedLength, text } = this.props;
+    return text.slice(0, collapsedLength);
+  }
+
+  render () {
+    return (
+      <div className="comment-text">
+        <FormattedText text={this.getVisibleText()} />
+        {this.canBeExpanded() &&
+         <button onClick={() => this.toggleExpanded()}>
+           {this.isExpanded() ? 'Show less' : 'Show more'}
+         </button>
+        }
+      </div>
+    );
+  }
+}
+
+export default connectToConfiguration(settings => ({
+  collapsedLength: settings.maxVisibleCommentLength,
+}))(CommentText)
